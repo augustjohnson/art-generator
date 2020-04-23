aee9f5b7cb0b01bad2d8e02d3c5f7a8826089cd6
August Johnson <august.johnson@gmail.com>
2020-04-23 13:13:48 -0500

    returning a basic svg

--- artgen/svg/views.py
+++ artgen/svg/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .svgGenerator import svgGenerator
 
-# Create your views here.
+
+def index(request):
+    svg = svgGenerator.basic()
+    return HttpResponse(svg)
